9468d5d4148935349d00357a3b0961229e1e0fd0
Kiran Malave <kiran.malave@gmail.com>
2026-02-27 14:29:46 +0530

    config change

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export const APP_NAME = "Iverify Karban";
 //export const API_BASE_URL = 'http://192.168.0.106:3000';
-export const API_BASE_URL = 'http://admin.iverifycarbon.com/API';
+export const API_BASE_URL = '/API';
