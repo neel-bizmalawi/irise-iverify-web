a4c5e086c801b652974b203b03ec9e88b103f755
Kiran Malave <kiran.malave@gmail.com>
2026-03-13 19:08:22 +0530

    crupto change

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 /* eslint-disable prettier/prettier */
+import { webcrypto } from "crypto";
+
+(global as any).crypto = webcrypto;
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
