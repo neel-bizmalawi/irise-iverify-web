2244bf2410873f2a0b0401b78a90a23f2712799e
Kiran Malave <kiran.malave@gmail.com>
2026-04-14 15:39:08 +0530

    add app url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const APP_NAME = "Iverify Karban";
 //export const API_BASE_URL = 'http://192.168.0.106:3000';
-export const API_BASE_URL = '/IV-API';
+export const API_BASE_URL = '/IV-LIVE-API';
